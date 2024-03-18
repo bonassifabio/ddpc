0fd37ad621d4d2683e88e23ffcf4498e26f1ca70
Fabio Bonassi <bonassi.fabio.94@gmail.com>
2024-03-18 08:58:47 +0100

    Updated license notice

--- gddpc/utils.py
+++ gddpc/utils.py
@@ -1,3 +1,22 @@
+'''
+Copyright (C) 2024 Fabio Bonassi and co-authors
+
+This file is part of gddpc.
+
+gddpc is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+gddpc is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gddpc.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import contextlib
 
 import numpy as np
